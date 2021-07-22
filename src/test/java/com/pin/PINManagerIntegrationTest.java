deb7a408ea59f6fe3585ed0e9cb9007c561d1679
manuel91 <manuelalejandro730@gmail.com>
2021-07-22 18:16:14 +0100

    Removing unused imports

--- src/test/java/com/pin/PINManagerIntegrationTest.java
+++ src/test/java/com/pin/PINManagerIntegrationTest.java
@@ -20,8 +20,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
