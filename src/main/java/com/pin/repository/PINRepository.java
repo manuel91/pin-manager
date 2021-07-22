6f731a46ac5ba4ad73a88a9089cb1afdf20f5b64
manuel91 <manuelalejandro730@gmail.com>
2021-07-22 14:07:50 +0100

    Adding minor fix

--- src/main/java/com/pin/repository/PINRepository.java
+++ src/main/java/com/pin/repository/PINRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface PINRepository extends CrudRepository<PIN, Long> {
 
-    List<PIN> findByCreationDateTimeLessThanEqualAndDiscardedFalse(LocalDateTime currentDateTime);
+    List<PIN> findByCreationDateTimeLessThanEqualAndDiscardedFalse(LocalDateTime dateTime);
 
 }
